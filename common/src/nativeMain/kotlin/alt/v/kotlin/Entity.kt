1a28101364ff6c61db5e8c40fecfd7bc914510c0
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-11-10 13:02:02 +0100

    Add entity data support for int64_t, Boolean, Double, String and uint64_t

--- common/src/nativeMain/kotlin/alt/v/kotlin/Entity.kt
+++ common/src/nativeMain/kotlin/alt/v/kotlin/Entity.kt
@@ -23,6 +23,26 @@ open class Entity internal constructor(
         open operator fun set(key: String, value: CValue<alt_mvalue_t>) {
             alt_entity_set_meta_data(pointer, key, value)
         }
+
+        open operator fun set(key: String, value: int64_t) {
+            alt_entity_set_meta_data(pointer, key, alt_mvalue_create_int(value))
+        }
+
+        open operator fun set(key: String, value: Boolean) {
+            alt_entity_set_meta_data(pointer, key, alt_mvalue_create_bool(value))
+        }
+
+        open operator fun set(key: String, value: Double) {
+            alt_entity_set_meta_data(pointer, key, alt_mvalue_create_double(value))
+        }
+
+        open operator fun set(key: String, value: String) {
+            alt_entity_set_meta_data(pointer, key, alt_mvalue_create_str(value))
+        }
+
+        open operator fun set(key: String, value: uint64_t) {
+            alt_entity_set_meta_data(pointer, key, alt_mvalue_create_uint(value))
+        }
     }
 
     class SyncedData(pointer: COpaquePointer) : Data(pointer) {
@@ -36,6 +56,26 @@ open class Entity internal constructor(
 
         override operator fun set(key: String, value: CValue<alt_mvalue_t>) {
             alt_entity_set_synced_meta_data(pointer, key, value)
+        }
+
+        override operator fun set(key: String, value: int64_t) {
+            alt_entity_set_synced_meta_data(pointer, key, alt_mvalue_create_int(value))
+        }
+
+        override operator fun set(key: String, value: Boolean) {
+            alt_entity_set_synced_meta_data(pointer, key, alt_mvalue_create_bool(value))
+        }
+
+        override operator fun set(key: String, value: Double) {
+            alt_entity_set_synced_meta_data(pointer, key, alt_mvalue_create_double(value))
+        }
+
+        override operator fun set(key: String, value: String) {
+            alt_entity_set_synced_meta_data(pointer, key, alt_mvalue_create_str(value))
+        }
+
+        override operator fun set(key: String, value: uint64_t) {
+            alt_entity_set_synced_meta_data(pointer, key, alt_mvalue_create_uint(value))
         }
     }
 
