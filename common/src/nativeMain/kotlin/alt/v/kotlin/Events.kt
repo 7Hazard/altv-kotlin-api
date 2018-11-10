b78c10afd964c278a863b21b8be0f1cafc6514a8
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-11-10 13:43:29 +0100

    Add basic events and player implementation

---
+++ common/src/nativeMain/kotlin/alt/v/kotlin/Events.kt
@@ -0,0 +1,23 @@
+package alt.v.kotlin
+
+import alt.v.c.*
+import kotlinx.cinterop.*
+import kotlin.reflect.KFunction1
+
+object Events {
+    private fun subscribe(event: alt_event_type_t, callback: alt_event_callback_t) {
+        alt_server_subscribe_event(event, callback)
+    }
+
+    fun subscribe(event: alt_event_type_t, callback: KFunction1<COpaquePointer, Unit>) {
+        subscribe(event, staticCFunction(callback) as alt_event_callback_t)
+    }
+
+    fun subscribe(callback: KFunction1<Player, Unit>) {
+        subscribe(EVENT_PLAYER_CONNECT, staticCFunction<COpaquePointer, Unit> { pointer ->
+            val playerPointer = alt_player_connect_event_get_target(pointer) ?: return@staticCFunction
+            val player = Player(playerPointer)
+            callback(player)
+        } as alt_event_callback_t)
+    }
+}
---
+++ common/src/nativeMain/kotlin/alt/v/kotlin/Player.kt
@@ -0,0 +1,15 @@
+package alt.v.kotlin
+
+import alt.v.c.alt_player_get_name
+import alt.v.c.alt_player_set_name
+import kotlinx.cinterop.toKString
+
+class Player internal constructor(pointer: kotlinx.cinterop.COpaquePointer) :
+    Entity(pointer) {
+    var name: String?
+        //decodeFromUtf8
+        get() = alt_player_get_name(pointer)?.toKString()
+        set(value) {
+            alt_player_set_name(pointer, value)
+        }
+}
