b67613edaf5554618d8895ebac5b7d1e7bb2029a
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-11-10 14:26:58 +0100

    Remove old comment

--- common/src/nativeMain/kotlin/alt/v/kotlin/Player.kt
+++ common/src/nativeMain/kotlin/alt/v/kotlin/Player.kt
@@ -7,7 +7,6 @@ import kotlinx.cinterop.toKString
 class Player internal constructor(pointer: kotlinx.cinterop.COpaquePointer) :
     Entity(pointer) {
     var name: String?
-        //decodeFromUtf8
         get() = alt_player_get_name(pointer)?.toKString()
         set(value) {
             alt_player_set_name(pointer, value)
