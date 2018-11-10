0b15937d17316a611ffaaa8c6fc5d3a53ad80d06
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-11-10 12:18:23 +0100

    Add entity (synced) meta data

--- common/src/nativeMain/kotlin/alt/v/kotlin/Entity.kt
+++ common/src/nativeMain/kotlin/alt/v/kotlin/Entity.kt
@@ -6,6 +6,26 @@ import kotlinx.cinterop.*
 open class Entity internal constructor(
     internal val pointer: kotlinx.cinterop.COpaquePointer
 ) {
+    open class Data(val pointer: COpaquePointer) {
+        open operator fun get(key: String): CValue<alt_mvalue_t>? {
+            return alt_entity_get_meta_data(pointer, key)
+        }
+
+        open operator fun set(key: String, value: CValue<alt_mvalue_t>) {
+            alt_entity_set_meta_data(pointer, key, value)
+        }
+    }
+
+    class SyncedData(pointer: COpaquePointer): Data(pointer) {
+        override operator fun get(key: String): CValue<alt_mvalue_t>? {
+            return alt_entity_get_synced_meta_data(pointer, key)
+        }
+
+        override operator fun set(key: String, value: CValue<alt_mvalue_t>) {
+            alt_entity_set_synced_meta_data(pointer, key, value)
+        }
+    }
+
     var dimension: uint16_t
         set(value) {
             alt_entity_set_dimension(pointer, value)
@@ -16,4 +36,6 @@ open class Entity internal constructor(
             alt_entity_set_position(pointer, value)
         }
         get() = alt_entity_get_position(pointer)
+    val data = Data(pointer)
+    val syncedData = SyncedData(pointer)
 }
