911311ea3ea2b0fb7518bac6acb6a7c3eecb76ef
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-11-13 20:17:03 +0100

    Add vm options

--- jvm-module/src/jvmModuleMain/kotlin/Main.kt
+++ jvm-module/src/jvmModuleMain/kotlin/Main.kt
@@ -9,8 +9,14 @@ external fun altvMain() {
     memScoped {
         val javaVMPointer = allocPointerTo<JavaVMVar>()
         val envPointer = allocPointerTo<JNIEnvVar>()
+        val javaVmOptions = allocArray<JavaVMOption>(1)
+        javaVmOptions[0].optionString = "-Djava.class.path=path".cstr.getPointer(this)
         val args = cValue<JavaVMInitArgs> {
+            version = JNI_VERSION_1_8
+            nOptions = 1
+            options = javaVmOptions
         }
+
         val rc = JNI_CreateJavaVM(javaVMPointer.ptr, envPointer.ptr as CValuesRef<COpaquePointerVar>/*(void**)&env*/, args.ptr)
         if (rc != JNI_OK) {
             //TODO: error logging cin.get()
