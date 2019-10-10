c98772ee6604d33855ce686fb3ca960e6952dd59
Hazard <hazard9909@gmail.com>
2019-10-10 07:08:04 +0200

    Get/Set player pos

--- src/main/kotlin/alt/v/kotlin/entities/Player.kt
+++ src/main/kotlin/alt/v/kotlin/entities/Player.kt
@@ -7,12 +7,27 @@ import alt.v.jvm.CAPI
 import alt.v.kotlin.math.Float3
 //import alt.v.kotlin.toAltPosition
 import jnr.ffi.Pointer
+import jnr.ffi.Struct
 
 class Player internal constructor(pointer: Pointer) {
     private val pointer: Pointer = pointer
 
     val name: String
         get() = AltStringView(CAPI.func.alt_IPlayer_GetName(pointer)).use { it.str() }
+
+    var pos: Float3
+        get() {
+            val s = CAPI.alt_Vector_float_3_PointLayout()
+            s.useMemory(CAPI.func.alt_IPlayer_GetPosition(pointer))
+            return Float3(s.x.get(), s.y.get(), s.z.get())
+        }
+        set(value) {
+            val s = CAPI.alt_Vector_float_3_PointLayout()
+            s.x.set(value.x)
+            s.y.set(value.y)
+            s.z.set(value.z)
+            CAPI.func.alt_IPlayer_SetPosition(pointer, Struct.getMemory(s))
+        }
 
 //    val name: String
 //        get() = fromCString(50) { bufferptr -> CAPI.func.alt_player_get_name(ptr, bufferptr) }
