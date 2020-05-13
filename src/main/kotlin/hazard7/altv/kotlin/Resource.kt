b149bdbbb7f2dd5a361f139a44df6945054b0f7a
Hazard <hazard9909@gmail.com>
2020-05-13 04:29:57 +0200

    Read Main-Class from JAR manifest, log unhandled events

--- src/main/kotlin/hazard7/altv/kotlin/Resource.kt
+++ src/main/kotlin/hazard7/altv/kotlin/Resource.kt
@@ -1,22 +1,30 @@
 package hazard7.altv.kotlin
 
-import hazard7.altv.jvm.*
+import hazard7.altv.jvm.CAPI
+import hazard7.altv.jvm.CAPIExtra
+import hazard7.altv.jvm.StringUtil
 import hazard7.altv.kotlin.events.Event
 import hazard7.altv.kotlin.events.PlayerConnectEvent
 import hazard7.altv.kotlin.events.PlayerDeathEvent
 import hazard7.altv.kotlin.events.PlayerDisconnectEvent
 import jnr.ffi.Pointer
 import jnr.ffi.Struct
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.io.File
-import java.net.URLClassLoader
 import java.net.URL
-import kotlinx.coroutines.*
+import java.net.URLClassLoader
+import java.util.jar.JarFile
 
-class Resource {
+
+class Resource internal constructor(resourceptr: Pointer) {
     companion object {
         internal val ptrmap = HashMap<Pointer?, Resource>()
     }
 
+//    private val resourceptr = pointer // not needed
+    val name = StringView { ptr -> CAPI.func.alt_IResource_GetName(resourceptr, ptr) }
     // js as client type by default
     var clientType = "js"
 
@@ -26,10 +34,10 @@ class Resource {
             CAPI.func.alt_String_Resize(Struct.getMemory(sinfo.type), clientType.length.toLong())
             sinfo.type.data.get().putString(0, clientType, clientType.length, StringUtil.UTF8)
             val curtype = CAPI.func.alt_String_CStr(Struct.getMemory(sinfo.type))
-            Log.info("[Kotlin-JVM] Set client type to '$curtype'")
+            Log.info("[Kotlin-JVM] Set client type to '$curtype' for '$name'")
 
             true
-        } catch (e: Exception)
+        } catch (e: Throwable)
         {
             Log.exception(e, "[Kotlin-JVM] Exception when making client resource")
             false
@@ -40,38 +48,40 @@ class Resource {
         ptrmap[resource] = this
 
         // Load jar
-        val name = StringView { ptr -> CAPI.func.alt_IResource_GetName(resource, ptr) }
-        val main = StringView { ptr -> CAPI.func.alt_IResource_GetMain(resource, ptr) }.split(':')
-        val jarpath = main[0]
-        val mainclass = main[1]
-
+        val jarpath = StringView { ptr -> CAPI.func.alt_IResource_GetMain(resource, ptr) }
         val jarfile = File("resources/$name/$jarpath")
         if (!jarfile.isFile) {
             Log.error("[Kotlin-JVM] Could not open '${jarfile.absolutePath}'")
-            false
+            return@StartFn false
         }
-        else {
-            try {
-                val child = URLClassLoader(
-                        arrayOf<URL>(jarfile.toURI().toURL()),
-                        this.javaClass.classLoader
-                )
-                val classToLoad = Class.forName(mainclass, true, child)
-                val method = classToLoad.getDeclaredMethod("main", Resource::class.java)
-                method.invoke(null, this)
 
-                true
-            } catch (e: Exception) {
-                Log.error("[Kotlin-JVM] Exception while loading resource '$name'"
-                        + "\n\t Message: " + e.localizedMessage
-                        + "\n\t Cause: " + e.cause
-                        + "\n\t Ext: " + e.toString()
-                        //+"\n\tStack trace: "
-                )
-                e.printStackTrace()
-
-                false
+        try {
+            val jar = JarFile(jarfile)
+            val mainclass = jar.manifest.mainAttributes.getValue("Main-Class")
+            if(mainclass == null) {
+                Log.error("[Kotlin-JVM] Could not get Main-Class from manifest")
+                return@StartFn false
             }
+
+            val child = URLClassLoader(
+                arrayOf<URL>(jarfile.toURI().toURL()),
+                this.javaClass.classLoader
+            )
+            val classToLoad = Class.forName(mainclass, true, child)
+            val method = classToLoad.getDeclaredMethod("main", Resource::class.java)
+            method.invoke(null, this)
+
+            true
+        } catch (e: Throwable) {
+            Log.error("[Kotlin-JVM] Exception while loading resource '$name'"
+                    + "\n\t Message: " + e.localizedMessage
+                    + "\n\t Cause: " + e.cause
+                    + "\n\t Ext: " + e.toString()
+                //+"\n\tStack trace: "
+            )
+            e.printStackTrace()
+
+            false
         }
     }
 
@@ -84,7 +94,7 @@ class Resource {
         try {
             val event = Event(eventptr)
 
-            when (event.capiType)
+            when (event.type)
             {
                 CAPI.alt_CEvent_Type.ALT_CEVENT_TYPE_PLAYER_CONNECT -> {
                     val ev = PlayerConnectEvent(eventptr)
@@ -115,6 +125,10 @@ class Resource {
                             }) { handler(ev) }
                     }
                 }
+
+                else -> {
+                    Log.warning("[Kotlin-JVM] Unhandled event ${event.type.name}")
+                }
             }
 
             !event.wasCancelled
@@ -141,13 +155,6 @@ class Resource {
     internal val on_remove_base_object = CAPIExtra.OnRemoveBaseObjectFn { resource, refobj ->
 
     }
-
-    private val pointer: Pointer
-    constructor(pointer: Pointer)
-    {
-        this.pointer = pointer
-    }
-
 
     ////// Events //////
     // Player Connect
